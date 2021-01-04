4a618c49313c47391e054dcf05cb9f6e302d485d
Will Johnston <chefwill614@gmail.com>
2021-01-04 10:07:03 -0500

    App Setup
    > Removed boilerplate files
    > Installed styled-components & type defs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { ReactElement } from 'react'
+import styled from 'styled-components'
+
+const ComponentWrapper = styled.div``
 
 export default function App(): ReactElement {
   return (
